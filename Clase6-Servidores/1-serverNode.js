518708340f433b241189a7a820afa770337bdeba
Sebastian <soancavil@gmail.com>
2025-01-13 23:39:53 -0300

    Desarrollo clase 6

--- Clase6-Servidores/1-serverNode.js
+++ Clase6-Servidores/1-serverNode.js
@@ -1,1 +1,16 @@
 //Desarrollar
+const http = require('node:http')
+
+//host:port
+const hostname = '127.0.0.1' //localhost
+const port = 8080
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hola mundillo!\n')
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
--- Clase6-Servidores/2-express/src/app.js
+++ Clase6-Servidores/2-express/src/app.js
@@ -1,10 +1,55 @@
-const express = require('express')
+//Importamos el modulo de express
+import express from 'express'
+
+//Creamos app de express
 const app = express()
 const port = 3000
-const path = require('path')
 
+import path from 'path'
+
+const users = [
+  { id: 1, username: 'username1' },
+  { id: 2, username: 'username2' },
+]
+
+//Accedamos desde el browser a: http://127.0.0.1:3000/saludo
+//el server nos va a responder con un saludito
+//req = request, res = response
 app.get('/saludo', (req, res) => {
-  // res.send('Hola mundooo!')
+  res.send('Hola mundooo!')
+})
+
+app.get('/', (request, response) => {
+  response.send('Este el home')
+})
+
+app.get('/usuarios', (request, response) => {
+  console.log('Se accede a /usuarios')
+  const { nombre, edad } = request.query
+
+  console.log(edad)
+  console.log(nombre)
+
+  response.send({ users })
+})
+
+app.get('/usuarios/:id', (request, response) => {
+  try {
+    console.log('Se accede a /usuarios/:id/')
+
+    console.log(request.query)
+
+    //Es exactamente lo mismo que const id = request.params.id
+    const { id: idString } = request.params
+    const id = Number(idString)
+    if (!id) throw new Error('Por favor coloque un número como id')
+    const user = users.find((user) => user.id === id)
+    response.send({ user: user || 'no se encontró el user especificado' })
+  } catch (error) {
+    response.status(400).send({
+      errorMesage: error.message,
+    })
+  }
 })
 
 app.get('/home', (req, res) => {
@@ -12,5 +57,5 @@ app.get('/home', (req, res) => {
 })
 
 app.listen(port, () => {
-  // console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
