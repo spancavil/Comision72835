8adf1a144c94b21f32405aef74f60ffdd5de4104
Sebastian <soancavil@gmail.com>
2025-01-15 19:52:42 -0300

    Added template clase 7

---
+++ Clase7-ExpressAvanzado/1-app/app.js
@@ -0,0 +1,46 @@
+import express from 'express'
+
+const app = express()
+
+//Para poder trabajar con JSON y que se parseen correctamente a formatos de objeto
+app.use(express.json())
+app.use(express.urlencoded())
+
+/**
+ * @typedef {Object} Gatito
+ * @property {string} name
+ * @property {string} raza
+ * @property {string} id
+ */
+
+const gatitos = []
+
+app.post('/v1/gatito', (req, res) => {
+  /**
+   * @type {Gatito}
+   */
+  const gatito = req.body
+
+  if (!gatito.name || !gatito.raza) {
+    return res.status(400).send({
+      message: 'Error al crear un gatito',
+      error: 'Valores incompletos',
+    })
+  }
+  gatito.id = gatitos.length + 1
+
+  gatitos.push(req.body)
+
+  return res.status(200).send({
+    message: 'Gatito creada',
+    data: { ...gatito },
+  })
+})
+
+app.get('/v1/home', (req, res) => {
+  res.status(200).send({ message: 'Bienvenido al home!' })
+})
+
+const server = app.listen(3006, () => {
+  console.info('Server listen on port 3006')
+})
