632c756f87472761572b10b3a519f64cbd742d7b
Sebastian <soancavil@gmail.com>
2025-01-27 22:30:23 -0300

    Desarrollo clase 10

--- Clase10-Websockets/project - ws/src/public/js/socket.js
+++ Clase10-Websockets/project - ws/src/public/js/socket.js
@@ -1,5 +1,4 @@
-/* const socket = io()
-console.log(socket)
+const socket = io()
 
 const form = document.getElementById('form')
 const input = document.getElementById('input')
@@ -8,7 +7,10 @@ const messages = document.getElementById('messages')
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   if (input.value) {
-    socket.emit('chat message', input.value)
+    socket.emit(
+      'chat message',
+      `${socket.id}: ${input.value} | ${new Date().toLocaleString()}`
+    )
     input.value = ''
   }
 })
@@ -17,5 +19,11 @@ socket.on('chat message', (msg) => {
   const item = document.createElement('li')
   item.textContent = msg
   messages.appendChild(item)
-  window.scrollTo(0, document.body.scrollHeight)
-}) */
+  // window.scrollTo(0, document.body.scrollHeight)
+})
+
+socket.on('history', (mensajes) => {
+  console.log(mensajes)
+
+  //Se recibe el array
+})
--- Clase10-Websockets/project - ws/src/server.js
+++ Clase10-Websockets/project - ws/src/server.js
@@ -10,10 +10,18 @@ const server = app.listen(config.PORT, () => {
 
 const io = new Server(server)
 
+const messages = []
+
 io.on('connection', (socket) => {
+  //Enviando el historial de mensajes
+  socket.emit('history', messages)
+
   console.log('a user connected')
+  console.log({ socketId: socket.id })
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg)
+    messages.push(msg)
+    //Emite a todos los clientes conectados. (el broadcast emitiría a todos MENOS al emisor original)
     io.emit('chat message', msg)
   })
   socket.on('disconnect', () => {
